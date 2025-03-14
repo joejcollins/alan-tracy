a17b766f4a30cdb9bc55d7c45a425da8e1201147
Joe J Collins <j.collins@zengenti.com>
2025-03-14 12:48:13 +0000

    Source the cargo env

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,5 +8,5 @@ make activate
 # Install Rust using rustup so we can have tex-fmt.
 # Once Debian trixie is available tex-fmt can be installed as a package.
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-whoami
+. "$HOME/.cargo/env"
 cargo install tex-fmt
